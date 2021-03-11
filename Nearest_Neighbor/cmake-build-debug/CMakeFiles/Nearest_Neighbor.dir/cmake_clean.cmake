065c731c0942fca8a16c00d9a7d10872c10a3c2a
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-03-10 19:55:59 -0800

    Push

---
+++ Nearest_Neighbor/cmake-build-debug/CMakeFiles/Nearest_Neighbor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nearest_Neighbor.dir/main.cpp.o"
+  "Nearest_Neighbor"
+  "Nearest_Neighbor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nearest_Neighbor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
