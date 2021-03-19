25a019ef7a4e5b43160d41bb0a725f9052b113b0
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-03-19 00:59:26 -0700

    Data

--- Nearest_Neighbor/main.cpp
+++ Nearest_Neighbor/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <set>
+#include <fstream>
 using namespace std;
 
 void nearestNeighborSearch(int data) {
@@ -24,15 +25,24 @@ void backwardElimination() {}
 
 int main() {
 
-    int file;
+    string file;
+    ifstream in;
     int numFeatures = 0;
     int numInstances = 0;
     int algorithm = 0;
+    int val = 0;
 
     cout << "Welcome to Cristina Lawson's Feature Selection Algorithm" << endl;
     cout << "Type in the name of the file to test: ";
     cin >> file;
     cout << endl << endl;
+
+    in.open(file);
+
+    if (!in.is_open()) {
+        cout << "Could not open file " << file << "." << endl;
+        exit(1);
+    }
 
     cout << "Type the number of the algorithm you want to run." << endl << endl;
     cout << "1) Forward Selection" << endl
@@ -40,12 +50,23 @@ int main() {
     cin >> algorithm;
     cout << endl << endl;
 
+    while (in >> val) {
+        if (val.length() != 5) {
+            cout << "Must be 5 character word." << endl;
+        }
+        else {
+            //dict.push_back(val);
+        }
+    }
+
     cout << "This dataset has " << numFeatures << " features (not including the class attribute), with " << numInstances << " instances." << endl << endl;
 
     cout << "Please wait while I normalize the data... ";
     cout << "Done!";
 
     cout << "Running nearest neighbor with all 4 features, using “leaving-one-out” evaluation, I get an accuracy of 75.4%";
+
+    in.close();
 
     return 0;
 }
